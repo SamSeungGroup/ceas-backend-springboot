e3ddb317cdb3bb11af91f04fed176f1551344122
kws9208 <kws9208@gmail.com>
2023-03-11 18:06:56 +0900

    refactor: /products get 인가

--- src/main/java/com/samseung/ceas/config/WebSecurityConfig.java
+++ src/main/java/com/samseung/ceas/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.samseung.ceas.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -27,6 +28,7 @@ public class WebSecurityConfig{
 			.and()
 			.authorizeHttpRequests()
 			.requestMatchers("/", "/users/**").permitAll()
+			.requestMatchers(HttpMethod.GET, "/products/**").permitAll()
 			.anyRequest().authenticated();
 		
 		http.addFilterAfter(jwtAuthenticationFilter, CorsFilter.class);
