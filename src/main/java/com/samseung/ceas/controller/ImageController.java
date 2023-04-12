e362b83a9afeee1eb9415bf1007305bb51bf980d
kws9208 <kws9208@gmail.com>
2023-04-12 11:24:20 +0900

    refactor: user 기본이미지 추가

--- src/main/java/com/samseung/ceas/controller/ImageController.java
+++ src/main/java/com/samseung/ceas/controller/ImageController.java
@@ -1,6 +1,7 @@
 package com.samseung.ceas.controller;
 
 import com.samseung.ceas.dto.ResponseDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.MediaType;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import com.samseung.ceas.model.Image;
 import com.samseung.ceas.service.ImageService;
 
+@Slf4j
 @RestController
 @RequestMapping("/images")
 public class ImageController {
@@ -20,7 +22,14 @@ public class ImageController {
     public ResponseEntity<?> getImage (@PathVariable("userId") String userId){
         try{
             Image image = imageService.findByUserId(userId);
-            UrlResource resource = new UrlResource("file:" + image.getStoredFileName());
+            String imgUrl;
+            if(image == null){
+                imgUrl = "file:src/main/resources/static/image/default.jpg";
+            }else{
+                imgUrl = "file:" + image.getStoredFileName();
+            }
+            log.info(imgUrl);
+            UrlResource resource = new UrlResource(imgUrl);
             return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(resource);
         }catch (Exception e){
             ResponseDto response = ResponseDto.builder().error(e.getMessage()).build();
